7212b244fa75e1b65367e8e57443d7f289ddb749
Cole Fortner <colefortner@gmail.com>
2021-07-25 14:38:14 -0400

    Add basic Registration component

---
+++ src/components/auth/Registration.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class Registration extends Component {
+    render() {
+        return (
+            <div>Registration goes here</div>
+        );
+    }
+}
+
