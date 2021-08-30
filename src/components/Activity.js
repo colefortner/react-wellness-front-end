fbeaaa4bb5fd8d5a501ffe0e4cef49fc711e388a
Cole Fortner <colefortner@gmail.com>
2021-08-29 23:09:41 -0400

    Add Activity component

---
+++ src/components/Activity.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddFoodForm from './AddActivityForm';
+
+const Foods = () => {
+  return (
+    <div style={{marginTop: '100px', fontSize: '60px'}}>
+      <AddActivityForm />
+    </div>
+  )
+}
+
+export default Foods;
